11f2d97db31d64763e9010844e8dcd0ae73d90d0
Greg S <gwstev1@gmail.com>
2017-09-05 11:45:34 -0400

    Added file to test variable assignment

---
+++ variables.py
@@ -0,0 +1,23 @@
+# Practicing with variables
+
+# Simple assignment
+savings = 514.86
+salary = 320.51
+
+# Multiple variables on one line
+savings, salary = 514.86, 320.51
+
+# Use underscores for multi-word variables
+# No variable type or ending ;
+manila_pop = 1780148
+manila_area = 16.56
+manila_pop_density = manila_pop / manila_area
+print(manila_pop_density)
+manila_pop += 1675
+
+# Doesn't change
+print(manila_pop_density)
+
+# Need to do equation again
+manila_pop_density = manila_pop / manila_area
+print(manila_pop_density)
