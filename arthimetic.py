bff8c5e9fbb36f5850322de5d9cb8e9c154cc333
Greg S <gwstev1@gmail.com>
2017-09-05 11:37:20 -0400

    Added arithmetic expression tests

---
+++ arthimetic.py
@@ -0,0 +1,19 @@
+# Practicing arithmetic
+
+# Simple mathematics
+print(3+1)
+print(3**2)
+print(9%2)
+print(15//4)
+print(-5//4)
+print(1 + 2*3)
+print(16/4)
+print(3 + 2.5)
+print(341.)
+print(int(49.7))
+print(float(3520+3239))
+print(0.1)
+print(0.1+0.1+0.1)
+
+# 4.445 * 10 ** 8
+print(4.445e8)
