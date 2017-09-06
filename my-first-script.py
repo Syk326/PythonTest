e473d5027327af34a9b3e19e2963813f2110aaef
Greg S <gwstev1@gmail.com>
2017-09-06 11:14:53 -0400

    Added another pre-made Udacity script, and updated the first one

--- my-first-script.py
+++ my-first-script.py
@@ -2,8 +2,8 @@
 
 def welcome_message(name):
 #Prints out a personalised welcome message
-    return "Welcome to this Python script, " + name + "!"
+    return "This is where it gets fun, " + name + "!"
 
 #Call the welcome message function with the input "Udacity Student"
 #and print the output
-print(welcome_message("Udacity Student"))
+print(welcome_message("Greg"))
---
+++ python-snake.py
@@ -0,0 +1,15 @@
+# sample python script by Udacity
+how_many_snakes = 6
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Philip and Charlie
+"""
+print(snake_string * how_many_snakes)
